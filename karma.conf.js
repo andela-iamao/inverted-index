d9eb485a283715b18e8921d6de27c67a1203f6bd
andela-iamao <inumidun.amao@andela.com>
2017-02-23 10:59:44 +0000

    feature(fix) : fix missing , bug

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
         base: 'Chrome',
         flags: ['--no-sandbox']
       }
-    }
+    },
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
