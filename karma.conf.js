0c77687a84241b43e2f174968561e23874a50862
Inumidun Amao <inumidun.amao@andela.com>
2017-02-23 12:45:44 +0000

    feature(add) : add coveralls coverage to karma and travis

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
+      'karma-coverage',
       'karma-jasmine'
     ],
 
@@ -44,8 +45,14 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+
+
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
 
     // web server port
     port: 9876,
