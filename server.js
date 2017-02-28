6580ed49fa0bbfb6edd0b3970b6b8f7fcb02f536
Inumidun Amao <inumidun.amao@andela.com>
2017-02-28 11:28:32 +0000

    feature(udpate): update port in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-const app = express()
+const app = express();
 
 app.use(express.static(path.join(__dirname, '/dist')))
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'dist/index.html'))
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
 
-app.listen(4000);
+app.listen(process.env.PORT || 5000);
